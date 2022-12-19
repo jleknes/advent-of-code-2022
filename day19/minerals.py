9d456b65e82d6ace54dd933aa0f643174005a7f1
Jørund Leknes <jorund.leknes@gmail.com>
2022-12-19 19:59:39 +0100

    refactor: day 19, some optimisation

--- day19/minerals.py
+++ day19/minerals.py
@@ -8,123 +8,81 @@ GEODE = 3
 blueprints = {}
 
 
-def simulate():
-    minutes_left = 24
-    robots, resources = [0] * 4, [0] * 4
-    robots[ORE] = 1
-
-    while minutes_left > 0:
-        # Hvis det er mulig å kjøpe en geode-robot, gjør det
-        if resources[ORE] > 1 and resources[OBSIDIAN] > 6:
-            resources[ORE] -= 2
-            resources[OBSIDIAN] -= 7
-            robots[GEODE] += 1
-        minutes_left -= 1
-        for i in range(len(robots)):
-            resources[i] += robots[i]
-
-    print(resources)
-
-
-# simulate()
-
-
 @cache
-def rec(minutes_left, blueprint_id, ore_robots, clay_robots, obsidian_robots, geode_robots, ore, clay, obsidian):
-
-    #pruning? 
-    # har vi noen heuristikker som kan si at denne metoden gir mindre utbytte enn beste kalkulerte mulighet så langt?
+def rec(minutes_left, blueprint_id, robots, resources):
+    ore_robots = robots[ORE]
+    clay_robots = robots[CLAY]
+    obsidian_robots = robots[OBSIDIAN]
+    geode_robots = robots[GEODE]
+    ore = resources[ORE]
+    clay = resources[CLAY]
+    obsidian = resources[OBSIDIAN]
 
     if minutes_left == 1:
-        #    if geode + geode_robots == 9:
-        #        print(ore + ore_robots, clay + clay_robots, obsidian + obsidian_robots, geode + geode_robots)
         return geode_robots
-    # må flyttes. mineralene kommer inn senere i prosessen.
-    # ore += ore_robots
-    # clay += clay_robots
-    # obsidian += obsidian_robots
-    # geode += geode_robots
-    max_ore_robots = max(blueprints[blueprint_id][ORE],blueprints[blueprint_id][CLAY],blueprints[blueprint_id][OBSIDIAN][0],blueprints[blueprint_id][GEODE][0])
+    max_ore_robots = max(
+        blueprints[blueprint_id][ORE],
+        blueprints[blueprint_id][CLAY],
+        blueprints[blueprint_id][OBSIDIAN][0],
+        blueprints[blueprint_id][GEODE][0],
+    )
     max_clay_robots = blueprints[blueprint_id][OBSIDIAN][1]
 
     options = []
     if ore >= blueprints[blueprint_id][GEODE][0] and obsidian >= blueprints[blueprint_id][GEODE][1]:
-        return rec(
-            minutes_left - 1,
-            blueprint_id,
-            ore_robots,
-            clay_robots,
-            obsidian_robots,
-            geode_robots + 1,
-            ore - blueprints[blueprint_id][GEODE][0] + ore_robots,
-            clay + clay_robots,
-            obsidian - blueprints[blueprint_id][GEODE][1] + obsidian_robots
-        )+ geode_robots
+        return (
+            rec(
+                minutes_left - 1,
+                blueprint_id,
+                (ore_robots, clay_robots, obsidian_robots, geode_robots + 1),
+                (
+                    ore - blueprints[blueprint_id][GEODE][0] + ore_robots,
+                    clay + clay_robots,
+                    obsidian - blueprints[blueprint_id][GEODE][1] + obsidian_robots,
+                ),
+            )
+            + geode_robots
+        )
 
     if ore >= blueprints[blueprint_id][OBSIDIAN][0] and clay >= blueprints[blueprint_id][OBSIDIAN][1]:
         options.append(
             rec(
                 minutes_left - 1,
                 blueprint_id,
-                ore_robots,
-                clay_robots,
-                obsidian_robots + 1,
-                geode_robots,
-                ore - blueprints[blueprint_id][OBSIDIAN][0] + ore_robots,
-                clay - blueprints[blueprint_id][OBSIDIAN][1] + clay_robots,
-                obsidian + obsidian_robots,                
-            )+ geode_robots
+                (ore_robots, clay_robots, obsidian_robots + 1, geode_robots),
+                (
+                    ore - blueprints[blueprint_id][OBSIDIAN][0] + ore_robots,
+                    clay - blueprints[blueprint_id][OBSIDIAN][1] + clay_robots,
+                    obsidian + obsidian_robots,
+                ),
+            )
+            + geode_robots
         )
-    if ore >= blueprints[blueprint_id][CLAY] and clay_robots<max_clay_robots:
+    if ore >= blueprints[blueprint_id][CLAY] and clay_robots < max_clay_robots:
         options.append(
             rec(
                 minutes_left - 1,
                 blueprint_id,
-                ore_robots,
-                clay_robots + 1,
-                obsidian_robots,
-                geode_robots,
-                ore - blueprints[blueprint_id][CLAY] + ore_robots,
-                clay + clay_robots,
-                obsidian + obsidian_robots
-            )+ geode_robots
+                (ore_robots, clay_robots + 1, obsidian_robots, geode_robots),
+                (ore - blueprints[blueprint_id][CLAY] + ore_robots, clay + clay_robots, obsidian + obsidian_robots),
+            )
+            + geode_robots
         )
-    if ore >= blueprints[blueprint_id][ORE] and ore_robots<max_ore_robots:
+    if ore >= blueprints[blueprint_id][ORE] and ore_robots < max_ore_robots:
         options.append(
             rec(
                 minutes_left - 1,
                 blueprint_id,
-                ore_robots + 1,
-                clay_robots,
-                obsidian_robots,
-                geode_robots,
-                ore - blueprints[blueprint_id][ORE] + ore_robots,
-                clay + clay_robots,
-                obsidian + obsidian_robots
-            )+ geode_robots
+                (robots[ORE] + 1, *robots[1:]),
+                (ore - blueprints[blueprint_id][ORE] + ore_robots, clay + clay_robots, obsidian + obsidian_robots),
+            )
+            + geode_robots
         )
     options.append(
-        rec(
-            minutes_left - 1,
-            blueprint_id,
-            ore_robots,
-            clay_robots,
-            obsidian_robots,
-            geode_robots,
-            ore + ore_robots,
-            clay + clay_robots,
-            obsidian + obsidian_robots
-        )+ geode_robots
+        rec(minutes_left - 1, blueprint_id, robots, (ore + ore_robots, clay + clay_robots, obsidian + obsidian_robots))
+        + geode_robots
     )
     return max(options)
-
-
-# robots, resources = [0] * 4, [0] * 4
-# robots[ORE] = 1
-start_time = time.time()
-# print(rec(23, 1, 1, 0, 0, 0, 0, 0, 0, 0))
-# print(rec(23, 2, 1, 0, 0, 0, 0, 0, 0, 0))
-print("--- %s seconds ---" % (time.time() - start_time))
 
 
 def read_blueprint(line):
@@ -135,6 +93,7 @@ def read_blueprint(line):
     blueprint[OBSIDIAN] = (int(re.findall(r"\d+", line)[3]), int(re.findall(r"\d+", line)[4]))
     blueprint[GEODE] = (int(re.findall(r"\d+", line)[5]), int(re.findall(r"\d+", line)[6]))
     return blueprint
+
 
 def read_input():
     blueprint_id = 0
@@ -148,21 +107,22 @@ def solve_part_one():
     total_quality = 0
     for blueprint_id in blueprints.keys():
         print("processing blueprint id:", blueprint_id)
-        geodes = rec(24, blueprint_id, 1, 0, 0, 0, 0, 0, 0)
+        geodes = rec(24, blueprint_id, (1, 0, 0, 0), (0, 0, 0))
         print(geodes)
         rec.cache_clear()
         total_quality += geodes * blueprint_id
 
-    print(total_quality)  
+    print(total_quality)
 
-def solve_part_two():   
+
+def solve_part_two():
     product = 1
     for blueprint_id in range(1, 4):
         print("processing blueprint id:", blueprint_id)
-        geodes = rec(32, blueprint_id, 1, 0, 0, 0, 0, 0, 0)
+        geodes = rec(32, blueprint_id, (1, 0, 0, 0), (0, 0, 0))
         print(geodes)
         rec.cache_clear()
-        product*=geodes
+        product *= geodes
         print(product)
 
 
--- day20/grove_positioning.py
+++ day20/grove_positioning.py
@@ -3,32 +3,80 @@ import fileinput
 numbers = []
 original_position = []
 
+
 def read_input():
     i = 0
     for line in fileinput.input():
-        number = int (line.strip())
-        # add as a tuple to avoid problems with duplicates
-        numbers.append((number,i))
-        original_position.append((number,i))
-        i+=1
+        number = int(line.strip())
+        # add as a tuple with position to avoid problems with duplicates
+        numbers.append((number, i))
+        original_position.append((number, i))
+        i += 1
 
 
 def solve_part_one():
-    for i in range (len(original_position)):
+    for i in range(len(original_position)):
         number = original_position[i]
         curr_position = numbers.index(number)
-        new_position = (curr_position+number[0])%(len(numbers))
-        if (curr_position+number[0]>len(numbers)):
-            new_position-=1
+        new_position = (curr_position + number[0]) % (len(numbers) - 1)
+        # if (curr_position+number[0]>len(numbers)):
+        #    new_position-=1
         numbers.pop(curr_position)
         # if element is popped before current position
-        #if curr_position<new_position: new_position-=1
-        if new_position==0: 
-            new_position=len(numbers)-1
-        print (number, curr_position, new_position)
-        numbers.insert(new_position,number)
-        print ("after ",i,numbers)
-    print (numbers)
+        # if curr_position<new_position: new_position-=1
+        if new_position == 0:
+            new_position = len(numbers)
+        # print(number, curr_position, new_position)
+        numbers.insert(new_position, number)
+        # print("after ", i, numbers)
+    print(numbers)
+    zero_index = 0
+    while numbers[zero_index][0] != 0:
+        zero_index += 1
+    print(zero_index)
+    # print(numbers[1000 + zero_index - 1][0], numbers[2000 + zero_index - 1], numbers[3000 + zero_index - 1])
+    # print(numbers[1000 - 1][0] + numbers[2000 - 1][0] + numbers[3000 - 1][0])
+    # print(numbers[1000 + zero_index][0] + numbers[2000 + zero_index][0] + numbers[3000 + zero_index][0])
+
+
+def solve_part_two():
+    for i in range(len(numbers)):
+        numbers[i] = (numbers[i][0] * 811589153, numbers[i][1])
+        original_position[i] = numbers[i]
+    print(numbers)
+    for j in range(10):
+        for i in range(len(original_position)):
+            number = original_position[i]
+            curr_position = numbers.index(number)
+            # new_position = (curr_position + number[0]) % (len(numbers) - 1)
+            new_position = curr_position + number[0]
+            if new_position >= len(numbers) - 1:
+                new_position %= len(numbers)
+            # elif new_position < 0:
+            #    new_position = (new_position % len(numbers)) * -1
+            # if (curr_position+number[0]>len(numbers)):
+            #    new_position-=1
+
+            # if element is popped before current position
+            # if curr_position<new_position: new_position-=1
+            # if new_position == 0:
+            #    new_position = len(numbers)
+            # print(number, curr_position, new_position)
+            if new_position > 0:
+                numbers.pop(curr_position)
+                numbers.insert(int(new_position) - 1, number)
+            # print("after ", i, numbers)
+        print("rounds", j, numbers)
+    print(numbers)
+    zero_index = 0
+    while numbers[zero_index][0] != 0:
+        zero_index += 1
+    print(zero_index)
+    # print(numbers[1000 + zero_index - 1][0], numbers[2000 + zero_index - 1], numbers[3000 + zero_index - 1])
+    # print(numbers[1000 - 1][0] + numbers[2000 - 1][0] + numbers[3000 - 1][0])
+    # print(numbers[1000 + zero_index][0] + numbers[2000 + zero_index][0] + numbers[3000 + zero_index][0])
+
 
 read_input()
 solve_part_one()
+solve_part_two()
