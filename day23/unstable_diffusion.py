9ea50de3bfabd1eb283e50dbadfb873661b0751c
Jørund Leknes <jorund.leknes@gmail.com>
2022-12-23 09:26:14 +0100

    WIP: day 23

---
+++ day23/unstable_diffusion.py
@@ -0,0 +1,17 @@
+NORTH = 0
+SOUTH = 1
+WEST = 2
+EAST = 3
+
+# (dy,dx)
+check_tiles = ([(-1,-1),(-1,0),(-1,1)],[(1,-1),(1,0),(1,1)],[(-1,-1),(0,-1),(1,-1)],[(-1,1),(0,1),(1,1)])
+
+def solve():
+    proposed_move = set()
+    for elf in elves:
+        #get position of elf
+        # check each direction
+        # first check if proposed positions are outside
+
+    # loop through proposed_moves, check if there are any collisions
+        
